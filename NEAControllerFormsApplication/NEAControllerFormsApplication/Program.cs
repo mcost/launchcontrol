69a20d4570d47c23c5658928e603f164a50a0670
Christian Costantino <christian@costantino.uk>
2024-10-30 20:00:32 +0100

    my first change.

--- NEAControllerFormsApplication/NEAControllerFormsApplication/Program.cs
+++ NEAControllerFormsApplication/NEAControllerFormsApplication/Program.cs
@@ -14,6 +14,7 @@ namespace NEAControllerFormsApplication
         [STAThread]
         static void Main()
         {
+            // this is my comment
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Form1());
