3d8dacd041606f2ac0f371a15ca09c1efdea9e6e
Christian Costantino <christian@costantino.uk>
2024-10-30 20:18:11 +0100

    added form2

--- NEAControllerFormsApplication/NEAControllerFormsApplication/Program.cs
+++ NEAControllerFormsApplication/NEAControllerFormsApplication/Program.cs
@@ -16,6 +16,7 @@ namespace NEAControllerFormsApplication
         {
             // this is my comment
             // other comment.
+            // blabla
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Form1());
